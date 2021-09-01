1a95c77f079249c0995117708b81693c5f5876f1
mvaughan <magnus.vaughan@gmail.com>
2021-09-01 21:35:20 +0100

    customised

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,17 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // brand: "#0fa9e6",
+        // brand: {
+        //   light: "#0fa9e6",
+        // },
+      },
+      // fontFamily: {
+      //   headline: "Tahoma",
+      // },
+    },
   },
   variants: {
     extend: {},
